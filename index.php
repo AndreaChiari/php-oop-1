63b30bcfe279ecea2b7d3770ac5c7e0bbc6f544e
AndreaChiari <chiariandrea94@gmail.com>
2023-02-20 16:01:03 +0100

    push_vardump_commented

--- index.php
+++ index.php
@@ -35,12 +35,12 @@ class Movie
     }
 }
 
-$space_odissey = new Movie('2001: Odissea nello spazio', 'Stanley Kubrick', 1968, 139,"fantascienza");
-$carnage = new Movie('Carnage', 'Roman Polansky', 2011, 79,"drammatico");
-$genre = new Movie('Interstellar','Cristopher Nolan',2014,169,'fantascienza')
+$space_odissey = new Movie('2001: Odissea nello spazio', 'Stanley Kubrick', 1968, 139, "fantascienza");
+$carnage = new Movie('Carnage', 'Roman Polansky', 2011, 79, "drammatico");
+$genre = new Movie('Interstellar', 'Cristopher Nolan', 2014, 169, 'fantascienza');
 
-var_dump($space_odissey);
-var_dump($carnage);
+/*var_dump($space_odissey);
+var_dump($carnage);*/
 
 echo $space_odissey->durata();
 echo $carnage->durata();
